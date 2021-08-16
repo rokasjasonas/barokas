3b14feef8cb3cf9be4391a49a5de9a8578b88474
Rokas Jasonas <rokas.jasonas@gmail.com>
2021-08-16 22:55:59 +0300

    Init DetailsFragment

---
+++ app/src/main/java/com/rokas/barokas/screen/details/view/DetailsFragment.kt
@@ -0,0 +1,21 @@
+package com.rokas.barokas.screen.details.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.rokas.barokas.R
+
+class DetailsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+    companion object {
+        fun newInstance() = DetailsFragment()
+    }
+}
