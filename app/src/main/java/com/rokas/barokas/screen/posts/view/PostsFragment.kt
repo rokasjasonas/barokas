c2d61898e2634ed091b02cf8921127057478b549
Rokas Jasonas <rokas.jasonas@gmail.com>
2021-08-16 23:51:55 +0300

    Init navigation component

--- app/src/main/java/com/rokas/barokas/screen/posts/view/PostsFragment.kt
+++ app/src/main/java/com/rokas/barokas/screen/posts/view/PostsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.rokas.barokas.R
 import com.rokas.barokas.screen.details.view.DetailsFragment
 
@@ -14,6 +15,14 @@ class PostsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_posts, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.setOnClickListener {
+            Navigation.findNavController(view)
+                .navigate(R.id.action_postsFragment_to_detailsFragment)
+        }
     }
 
     companion object {
