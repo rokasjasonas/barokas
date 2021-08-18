e3361cbedd6c4837708bc5042a72d2298f42f492
Rokas Jasonas <rokas.jasonas@gmail.com>
2021-08-19 01:09:19 +0300

    Update DetailsFragment to use view bindings

--- app/src/main/java/com/rokas/barokas/screen/details/view/DetailsFragment.kt
+++ app/src/main/java/com/rokas/barokas/screen/details/view/DetailsFragment.kt
@@ -1,23 +1,32 @@
 package com.rokas.barokas.screen.details.view
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.rokas.barokas.R
+import com.rokas.barokas.base.BaseFragment
+import com.rokas.barokas.databinding.FragmentDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 import timber.log.Timber
 
 @AndroidEntryPoint
-class DetailsFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+class DetailsFragment : BaseFragment<FragmentDetailsBinding>(R.layout.fragment_details) {
+    private var disposable = CompositeDisposable()
+
+    override fun bindView(view: View) = FragmentDetailsBinding.bind(view)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         val postId = arguments?.getInt(KEY_POST_ID)
         Timber.e("xx postId " + postId)
-        return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+    override fun setUpObservers() {
+    }
+
+    override fun onStop() {
+        disposable.clear()
+        super.onStop()
     }
 
     companion object {
